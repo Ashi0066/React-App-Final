505c893afdd367f837e7ad8c1dd1e908aaf26f80
Muhammad Ashir <108072595+Ashi0066@users.noreply.github.com>
2024-03-14 23:07:12 +0000

    new commit 2

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://scaling-robot-wpvxg76wjw439jqr-3000.app.github.dev/api";
+const API_URL = "https://special-carnival-p479prvx67pf9v4-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
